20baef2c80d80b6f2d3cc5d2ef8480fd7529a798
Aarthi <aarthi.m@keka.com>
2025-07-19 16:40:58 +0530

    Undoing changes

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -7,7 +7,6 @@ test('has title', async ({ page }) => {
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-//GO to link
 test('get started link', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
